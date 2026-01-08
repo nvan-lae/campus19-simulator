b3136cc81d06c9fbeac48a128859dbb0fbb260e3
Noah Van Laer <noahvanlaer@gmail.com>
2026-01-08 16:52:27 +0100

    enabled Cors so frontend can talk to backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
